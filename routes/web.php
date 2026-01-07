4d17981f88784c71092e397bbfb6f577c536e65b
menegass0 <victormenegasso@unisantos.br>
2026-01-07 17:38:07 -0300

    inicio do projeto conexao do laravel com vue atraves do Inertia

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Index');
+});
+
+Route::get('/teste', function () {
+    return Inertia::render('Test');
+});
